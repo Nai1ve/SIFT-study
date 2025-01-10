53904411b0b3ce1b00d9848c078a5b0936687318
TeMenbaby(Mac) <Lpz970821@163.com>
2025-01-10 10:43:35 +0800

    更新了.gitignore文件，并修改了gaussian_pyramid.py和sift.py的代码内容。

--- gaussian_pyramid.py
+++ gaussian_pyramid.py
@@ -8,7 +8,7 @@ def gaussian_blur(image, times=2):
 
 def main():
     # 读取原始图像
-    input_image = cv2.imread('zeeta.JPEG')  # 替换为你的图像路径
+    input_image = cv2.imread('Zeeta.jpg')  # 替换为你的图像路径
 
     # 确保输入图像不为空
     if input_image is None:
@@ -35,13 +35,27 @@ def main():
         blurred_image_2 = gaussian_blur(blurred_image_1, times=1)  # 第二次模糊
 
         # 纵向拼接未模糊的图像和模糊结果
-        combined_image = np.vstack((original_image, blurred_image_1, blurred_image_2))
-        output_images.append(combined_image)
+        # combined_image = np.vstack((original_image, blurred_image_1, blurred_image_2))
+        output_images.append(original_image)
+        output_images.append(blurred_image_1)
+        output_images.append(blurred_image_2)
 
     # 显示和保存结果图像
     for i, image in enumerate(output_images):
         cv2.imshow(f'Image {i + 1}', image)
         cv2.imwrite(f'output_image_{i + 1}.jpg', image)  # 保存图像为文件
+
+    # 三个为一组，做高斯差分金字塔
+    for i in range(0, len(output_images), 3):
+        diff_image1 = cv2.absdiff(output_images[i], output_images[i+1])
+        diff_image2 = cv2.absdiff(output_images[i+1], output_images[i+2])
+        # 差分后对图像进行归一化处理
+        diff_image1 = cv2.normalize(diff_image1, None, 0, 255, cv2.NORM_MINMAX)
+        diff_image2 = cv2.normalize(diff_image2, None, 0, 255, cv2.NORM_MINMAX)
+        cv2.imshow(f'Image {i + 1}', diff_image1)
+        cv2.imshow(f'Image {i + 2}', diff_image2)
+        cv2.imwrite(f'output_dog_image_{i + 1}.jpg', diff_image1 )  # 保存图像为文件
+        cv2.imwrite(f'output_dog_image_{i + 2}.jpg', diff_image2 )
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
